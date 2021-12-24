b7f1621390461aa5a4e44215b8c8dedf942ee2fc
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2021-12-24 15:11:57 +0700

    change function

--- fri_ex1.rb
+++ fri_ex1.rb
@@ -4,22 +4,17 @@ p = Proc.new do |i|
     puts  "gia tri proc :#{i}"
   end
 end
+def fun1(arr, &p)
+   yield arr
+end
+fun1([1,2,3,4], &p) 
 l = -> (i) do 
   i.each do |x|
     j= x*3
     puts  "gia tri lamdba :#{j}"
   end
 end
-def fun1
-  yield([1,2,3,4])
+def fun2(arr, &l)
+  yield (arr)
 end
-# def fun1(arr) 
-#    yield(arr)
-# end
-def fun2
-  yield ([1,3,5,7])
-end
-#fun1([1,2,3]) &p
-fun1 &p
-# fun2 &l   #lam sao de yield nhan 1 tham so??
-
+fun1([1,2,3,4], &l) 
